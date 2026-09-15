e7e23587946df0a2dda727ed4c503107d35dca35
aaaryanowns <antonellababystore2017@gmail.com>
2026-09-16 02:48:20 +0530

    app.py

--- app.py
+++ app.py
@@ -680,4 +680,10 @@ def create_app():
 app = create_app()
 
 if __name__ == "__main__":
-    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
+    socketio.run(
+        app,
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False,
+        allow_unsafe_werkzeug=True
+    )
